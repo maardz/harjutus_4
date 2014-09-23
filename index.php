e7dad39eb83bca6f822dc85ab9421f61ff6bb9ae
maardz <moldermarge@gmail.com>
2014-09-23 16:54:06 +0300

    Add number functions

--- index.php
+++ index.php
@@ -5,7 +5,27 @@
 	<title></title>
 </head>
 <body>
+	<h2>Arvutamine murd- ja täisarvudega</h2>
 
+	<?php echo $float = 3.14; 
+		echo "<br>";
+		echo $float + 7;
+		echo "<br>";
+		echo 4/3;
+
+	?><br>
+
+	<h2>Murdarvude ümardamine</h2>
+	<?php
+		$float = 3.14; 
+		echo round($float);
+		echo "<br>";
+		echo ceil($float);
+		echo "<br>";
+		echo floor($float);
+		
+
+	?><br>
 
 </body>
 </html>
